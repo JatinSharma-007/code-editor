3d39c548fa5b7bee7231531cda4f1e6793aa96db
JatinSharma-007 <53529241+JatinSharma-007@users.noreply.github.com>
2022-03-26 00:56:56 +0530

    added build version

--- src/pages/EditorPage.jsx
+++ src/pages/EditorPage.jsx
@@ -98,7 +98,7 @@ const EditorPage = () => {
                     <div className="logo">
                         <img
                             className="logoImage"
-                            src="/code-sync.png"
+                            src="/logo.png"
                             alt="logo"
                         />
                     </div>
