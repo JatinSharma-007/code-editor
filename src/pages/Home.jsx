87918b8bff57617b73aa1094b4fcb483d96a7bb4
JatinSharma-007 <53529241+JatinSharma-007@users.noreply.github.com>
2022-03-26 00:46:03 +0530

    editor done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,69 +1,75 @@
-import React, { useState } from "react";
-import { v4 as uuidv4 } from "uuid";
-import toast from "react-hot-toast";
-import { useNavigate } from "react-router-dom";
+import React, { useState } from 'react';
+import { v4 as uuidV4 } from 'uuid';
+import toast from 'react-hot-toast';
+import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
     const navigate = useNavigate();
-    const [roomId, setRoomId] = useState("");
-    const [username, setUsername] = useState("");
 
+    const [roomId, setRoomId] = useState('');
+    const [username, setUsername] = useState('');
     const createNewRoom = (e) => {
         e.preventDefault();
-        const id = uuidv4();
+        const id = uuidV4();
         setRoomId(id);
-        toast.success("New Room Created");
+        toast.success('Created a new room');
     };
+
     const joinRoom = () => {
         if (!roomId || !username) {
-            toast.error("Enter Room Id & Username");
+            toast.error('Enter Room Id & Username');
             return;
         }
 
+        // Redirect
         navigate(`/editor/${roomId}`, {
             state: {
                 username,
             },
         });
     };
+
     const handleInputEnter = (e) => {
-        if (e.code === "Enter") {
+        if (e.code === 'Enter') {
             joinRoom();
         }
     };
     return (
         <div className="homePageWrapper">
             <div className="formWrapper">
-                <img className="homePageLogo" src="/logo.png" alt="code-editor-logo" />
-                <h4 className="mainLabel">CREATE OR JOIN ROOM</h4>
+                <img
+                    className="homePageLogo"
+                    src="/logo.png"
+                    alt="code-editor"
+                />
+                <h4 className="mainLabel">ENTER ROOM ID</h4>
                 <div className="inputGroup">
                     <input
                         type="text"
-                        value={roomId}
-                        onChange={(e) => {
-                            setRoomId(e.target.value);
-                        }}
-                        onKeyUp={handleInputEnter}
                         className="inputBox"
                         placeholder="ENTER ROOM ID"
+                        onChange={(e) => setRoomId(e.target.value)}
+                        value={roomId}
+                        onKeyUp={handleInputEnter}
                     />
                     <input
                         type="text"
-                        value={username}
-                        onChange={(e) => {
-                            setUsername(e.target.value);
-                        }}
-                        onKeyUp={handleInputEnter}
                         className="inputBox"
-                        placeholder="USERNAME"
+                        placeholder="ENTER USERNAME"
+                        onChange={(e) => setUsername(e.target.value)}
+                        value={username}
+                        onKeyUp={handleInputEnter}
                     />
                     <button className="btn joinBtn" onClick={joinRoom}>
                         Join
                     </button>
                     <span className="createInfo">
-                        If you don't have an invite then create
-                        <a onClick={createNewRoom} href="" className="createNewBtn">
-                            {" "}
+                        If you don't have an invite then create &nbsp;
+                        <a
+                            onClick={createNewRoom}
+                            href=""
+                            className="createNewBtn"
+                        >
                             new room
                         </a>
                     </span>
@@ -71,8 +77,8 @@ const Home = () => {
             </div>
             <footer>
                 <h4>
-                    Built with ❤️ By{" "}
-                    <a href="https://github.com/JatinSharma-007"> Jatin Sharma </a>{" "}
+                    Built with ❤️ &nbsp; by &nbsp;
+                    <a href="https://github.com/JatinSharma-007/">Jatin Sharma</a>
                 </h4>
             </footer>
         </div>
